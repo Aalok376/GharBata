7d439c5f957dd438d370307134c22299914d0c08
Sanik Karmacharya <sanikkarmacharya1000@gmail.com>
2025-05-28 11:47:46 +0545

    adding icons in navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import {FaHome,FaUserPlus,FaSignInAlt,FaPhoneAlt} from "react-icons/fa";
 
 const Navbar=()=>{
     const[isSignUpOpen,setIsSignUpOpen]=useState(false);
     const [isLoginOpen,setIsLoginOpen]=useState(false);
     return(
-        <nav className="bg-blue-600 text-white p-4 shadow-md">
+        <nav className="bg-blue-900 text-white p-4 shadow-md sticky top-0 z-50">
             <div className="container mx-auto flex justify-between items-center">
                 {/*Left side:Logo*/}
                 <div className="text-2xl font-bold"> 
@@ -13,27 +14,34 @@ const Navbar=()=>{
                 </div>
                 {/*Right side:Navigation Links*/}
                 <div className="flex items-center space-x-6 ">
-                    <Link to='/' className='hover:text-gray-200'>Home</Link>
+                    <Link to='/' className='flex items-center gap-1 hover:text-gray-200'>
+                    <FaHome/>
+                    Home
+                    </Link>
                     {/*SignUp Dropdown */}
                     <div 
                     className="relative"
                     onMouseEnter={()=>setIsSignUpOpen(true)}
                     onMouseLeave={()=>setIsSignUpOpen(false)}
                     >
-                        <button className="hover:text-gray-200 focus:outline-none">SignUp</button>
+                        <button className="flex items-center gap-1 hover:text-gray-200 focus:outline-none cursor-pointer">
+                            <FaUserPlus/>
+                            SignUp
+                            </button>
                       
-                            <div className={`absolute right-0 mt-2 w-40 bg-green-600 text-black rounded-md shadow-lg z-10  transition-all duration-300 ${
+                            <div
+                             className={`absolute right-0 mt-2 w-40 bg-white text-gray-800 rounded-md shadow-lg z-10  transition-all duration-300 ${
                                 isSignUpOpen ? "opacity-100 visible" : "opacity-0 invisible"
                             }`}>
                                 <Link
                                 to="/client_signup"
-                                className="block px-4 py-2 hover:bg-gray-100"
+                                className="block px-4 py-2 hover:bg-blue-100"
                                 >
                                     User
                                 </Link>
                                 <Link 
                                 to="/technician_signup"
-                                className="block px-4 py-2 hover:bg-gray-100"
+                                className="block px-4 py-2 hover:bg-blue-100"
                                 >
                                     Technician
                                 </Link>
@@ -46,22 +54,24 @@ const Navbar=()=>{
                     onMouseEnter={()=>setIsLoginOpen(true)}
                     onMouseLeave={()=>setIsLoginOpen(false)}
                     >
-                        <button className="hover:text-gray-200 focus:outline-none">
+                        <button className="flex items-center gap-1 hover:text-gray-200 focus:outline-none cursor-pointer">
+                            <FaSignInAlt/>
                             Login
                         </button>
                         
-                            <div className={`absolute right-0 mt-2 w-40 bg-green-600 text-black rounded-md shadow-lg z-10 transition-all duration-300 ${
+                            <div 
+                            className={`absolute right-0 mt-2 w-40 bg-white text-gray-800 rounded-md shadow-lg z-10 transition-all duration-300 ${
                                 isLoginOpen ? "opacity-100 visible":"opacity-0 invisible"
                             }`}>
                                 <Link
                                 to="/client_login"
-                                className="block px-4 py-2 hover:bg-gray-100"
+                                className="block px-4 py-2 hover:bg-blue-100"
                                 >
                                     User
                                 </Link>
                                 <Link
                                 to="/technician_login"
-                                className="block px-4 py-2 hover:bg-gray-100"
+                                className="block px-4 py-2 hover:bg-blue-100"
                                 >
                                     Technician
                                 </Link>
@@ -69,7 +79,12 @@ const Navbar=()=>{
                             </div>
                         
                     </div>
-                    <Link to="/contact" className="hover:text-gray-200">Contact Us</Link>
+                    <Link
+                     to="/contact" 
+                     className="flex items-center gap-1 hover:text-gray-200">
+                        <FaPhoneAlt/>
+                        Contact Us
+                        </Link>
                 </div>
             </div>
         </nav>
