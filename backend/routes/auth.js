db587a28cdf32b013e0bc668648947487065241d
ASU-N <asapkotaupadhyaya@gmail.com>
2025-06-16 13:38:02 +0545

    login signup backend (forgot to add the chanded file previously)

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,12 @@
 import express from 'express';
 import { register, login, verifyEmail } from '../controllers/authController.js';
 
+import { authMiddleware } from './middleware/auth.js';
+
+app.get('/api/protected', authMiddleware, (req, res) => {
+  res.json({ message: `Hello, ${req.user.email}` });
+});
+
 const router = express.Router();
 
 router.post('/register', register);
