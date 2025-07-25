ef9b39f00904e9b7f3966e11798d767eab93cd60
Aalok376 <yadavaalok453@gmail.com>
2025-07-25 22:16:40 +0545

    sanik

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,30 @@
 import React, { useEffect, useState } from 'react';
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
 
 import HomePageFunc from './pages/IntroductionPage'
 
 import Client_Login from './pages/Client_Login'
 import Client_Signup from './pages/Client_Signup'
-import ClientPage from './pages/clientHomePage'
+import ClientPage from './pages/clientHomePage'  // only one import here
 
 import Technician_Signup from './pages/Technician_Signup'
 import Technician_Login from './pages/Technician_Login'
 import ProfessionalPage from './pages/professionalHomePage'
-import ClientPage from './pages/clientHomePage'
 import Overlay_Otp from './components/otpOverlay'
 import ChatPage from './pages/Chat_page'
 
 import ProfessionalProfilePage from './pages/professionalProfile'
 import BookingForm from './pages/bookingform'
 import TechnicianDisplayPage from './pages/professionalList'
-import MapPickerModal from './pages/MapPicker'
-import { SelectLocationOverlay } from './components/selectLocation'
 import ClientProfilePage from './pages/clientProfilePage'
-import OtherClientProfile from './pages/otherClientProfile'
 import BookingDashboard from './components/BookingDashboard'
-import  CreateBooking from './components/CreateBooking';
-import PaymentPage from './pages/PaymentPage';
-import PaymentSuccess from './components/PaymentSuccess';
-import PaymentFailure from './components/PaymentFailure';
-import PaymentStatus from './components/PaymentStatus';
+import CreateBooking from './components/CreateBooking'
+import PaymentPage from './pages/PaymentPage'
+import PaymentSuccess from './components/PaymentSuccess'
+import PaymentFailure from './components/PaymentFailure'
+import PaymentStatus from './components/PaymentStatus'
 
-import MapPickerModal from './components/MapPicker'
 
 
 function App(){
@@ -62,8 +58,6 @@ function App(){
            <Route path='/payment/success' element={<PaymentSuccess/>}/>
             <Route path="/payment/failure" element={<PaymentFailure />} />
             <Route path="/payment/status" element={<PaymentStatus />} />
-
-        <Route path='/maps' element={<MapPickerModal/>}/>
       </Routes>
     
 
