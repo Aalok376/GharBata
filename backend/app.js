a9c5895e4ee65192824749c43bf5eb3f54cb7083
SameerKumarSah <sameersah101@gmail.com>
2025-08-06 21:27:16 +0545

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -21,6 +21,7 @@ app.use(cors({
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
+app.set('trust proxy', 1)
 
 app.use(session({
   secret: process.env.SESSION_SECRET,
